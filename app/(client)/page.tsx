87316ef0bc37bc7f07d55d70f85bfa84800d3f66
Henderson Dike-Benard <hendersondike@gmail.com>
2025-01-04 12:35:25 +0100

    add Hero component and integrate motion library for animations

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "../components/Hero";
 
 
 export default function Home() {
   return (
-   <div>Homepage</div>
+    <div className="mx-auto p-6 sm:p-8 lg:p-12">
+      <Hero />
+    </div>
   );
 }
---
+++ app/components/Hero.tsx
@@ -0,0 +1,37 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import heroimage from "../../app/assets/Hero.jpeg"
+import { motion } from 'framer-motion'
+
+export default function Hero() {
+    return (
+        <section className="mx-auto max-w-2xl px-4 sm:p-8 lg:p-2">
+            <div className="flex flex-col items-center text-center">
+                <div className="relative w-full h-64 sm:h-80 rounded-lg overflow-hidden mb-4">
+                    <Image
+                        src={heroimage}
+                        alt="Colorful hoodies on hangers including white, orange, and green"
+                        className="object-cover"
+                        fill
+                        priority
+                    />
+                </div>
+                <h1 className="text-3xl sm:text-4xl lg:text-4xl font-semibold mb-6 tracking-tight font-inter text-[#4F4D4D]">
+                    Get the best quality merch for much lesser with the ability to customize.
+                </h1>
+                <motion.button
+                    whileHover={{ y: -5 }}
+                    transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                >
+                    <Link
+                        href="/shop"
+                        className="inline-block font-bold bg-black text-white hover:bg-[#43EA7A] hover:text-black rounded-full px-8 py-3 transition-colors duration-200 font-inter"
+                    >
+                        Shop now
+                    </Link>
+                </motion.button>
+            </div>
+        </section>
+    )
+}
